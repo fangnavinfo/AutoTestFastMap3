eae4eaac483b1d00b5d3334363ca60b03bfba410
fangnavinfo <fangshishuai@navinfo>
2018-05-21 18:14:58 +0800

    增加http上传的过程

--- adapter_appium/src/com/fang/testAdapter/testadapter.java
+++ adapter_appium/src/com/fang/testAdapter/testadapter.java
@@ -7,7 +7,10 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -37,6 +40,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.openqa.selenium.By;
@@ -301,6 +305,80 @@ public class testadapter
             }  
 		} 
 	}
+	
+	public static void UpLoadFileToFastMap(String fileName) throws Exception 
+	{  
+        try 
+        {  
+            // 换行符  
+            final String newLine = "\r\n";  
+            final String boundaryPrefix = "--";  
+            // 定义数据分隔线  
+            String BOUNDARY = "----7d4a6d158c9";  
+            // 服务器的域名  
+            URL url = new URL(FASTMAP_URL + "/upload");  
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();  
+            // 设置为POST情  
+            conn.setRequestMethod("POST");  
+            // 发送POST请求必须设置如下两行  
+            conn.setDoOutput(true);  
+            conn.setDoInput(true);  
+            conn.setUseCaches(false);  
+            // 设置请求头参数  
+            conn.setRequestProperty("connection", "Keep-Alive");  
+            conn.setRequestProperty("Charsert", "UTF-8");  
+            conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);  
+  
+            OutputStream out = new DataOutputStream(conn.getOutputStream());  
+  
+            // 上传文件  
+            File file = new File("./temp/" + fileName);  
+            StringBuilder sb = new StringBuilder();  
+            sb.append(boundaryPrefix);  
+            sb.append(BOUNDARY);  
+            sb.append(newLine);  
+            // 文件参数,photo参数名可以随意修改  
+            sb.append("Content-Disposition: form-data;name=\"files[]\";filename=\"" + fileName  
+                    + "\"" + newLine);  
+            sb.append("Content-Type:application/octet-stream");  
+            // 参数头设置完以后需要两个换行，然后才是参数内容  
+            sb.append(newLine);  
+            sb.append(newLine);  
+  
+            // 将参数头的数据写入到输出流中  
+            out.write(sb.toString().getBytes());  
+  
+            // 数据输入流,用于读取文件数据  
+            DataInputStream in = new DataInputStream(new FileInputStream(file));  
+            byte[] bufferOut = new byte[1024];  
+            int bytes = 0;  
+            // 每次读1KB数据,并且将文件数据写入到输出流中  
+            while ((bytes = in.read(bufferOut)) != -1) {  
+                out.write(bufferOut, 0, bytes);  
+            }  
+            // 最后添加换行  
+            out.write(newLine.getBytes());  
+            in.close();  
+  
+            // 定义最后数据分隔线，即--加上BOUNDARY再加上--。  
+            byte[] end_data = (newLine + boundaryPrefix + BOUNDARY + boundaryPrefix + newLine)  
+                    .getBytes();  
+            // 写上结尾标识  
+            out.write(end_data);  
+            out.flush();  
+            
+            // 读取服务器返回结果  
+            InputStream is = conn.getInputStream();  
+            InputStreamReader isr = new InputStreamReader(is, "utf-8");  
+            BufferedReader br = new BufferedReader(isr);  
+            String result = br.readLine();  
+            is.close();    
+            //out.close();  
+  
+        } catch (Exception e) {  
+        	throw e;
+        }  
+    }  
 	
 	public static void CreateMainBoard() throws IOException
 	{
@@ -668,7 +746,7 @@ public class testadapter
 	public static void StopApp() 
 	{
 		// TODO Auto-generated method stub
-		driver.close();
+		driver.resetApp();
 	}
 
 	public static void ClearWal()
